1c00b335ab612992b88aa4767d977238a98430d3
Jeroen Akkerman <jeroen.akkerman@easyflex.nl>
2019-04-15 17:43:30 +0200

    Make parameter name more verbose.

--- src/functions/uniquify.ts
+++ src/functions/uniquify.ts
@@ -1,3 +1,3 @@
 export function uniquifyArray<T>(array: T[]): T[] {
-    return array.filter((elem, index, self) => index === self.indexOf(elem));
+    return array.filter((element, index, self) => index === self.indexOf(element));
 }
