aaa07c7487f15504bdec53e9ad1657f53cd28673
Jeroen Akkerman <jeroen.akkerman@easyflex.nl>
2019-04-15 17:44:41 +0200

    Change filename to match function name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { generateNumbersArray } from './functions/generate-numbers-array';
-export { uniquifyArray } from './functions/uniquify';
+export { uniquifyArray } from './functions/uniquify-array';
 export { objectsArrayToObject } from './functions/objects-array-to-object';
 export { sortArrayByObjectProperty } from './functions/sort-array-by-object-property';
