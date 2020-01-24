60307b84cb03b681e3fb7c3557912235f0e1124f
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2020-01-24 12:40:14 +0100

    Add new exports to index.ts test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,17 @@
-import { generateNumbersArray, objectsArrayToObject, sortArrayByObjectProperty, uniquifyArray } from './index';
+import {
+    generateNumbersArray,
+    groupArrayByObjectProperty,
+    objectsArrayToObject,
+    sortArrayByObjectProperty,
+    uniquifyArray,
+    uniquifyObjectsArray,
+} from './index';
 
 test('exports', () => {
     expect(generateNumbersArray).toBeTruthy();
     expect(objectsArrayToObject).toBeTruthy();
     expect(sortArrayByObjectProperty).toBeTruthy();
     expect(uniquifyArray).toBeTruthy();
+    expect(uniquifyObjectsArray).toBeTruthy();
+    expect(groupArrayByObjectProperty).toBeTruthy();
 });
