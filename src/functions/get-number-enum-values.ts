980984eff8195965d7fbb6109a3346f9d2600667
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2022-10-02 23:40:44 +0200

    Update dependencies

--- src/functions/get-number-enum-values.ts
+++ src/functions/get-number-enum-values.ts
@@ -2,11 +2,12 @@
  * Get the key names from a Typescript number-Enum.
  * @param numberEnum - The enum to get the key names from.
  */
-export const getNumberEnumKeys = <T>(numberEnum: T): Array<keyof T> => Object.values(numberEnum).filter((x) => isNaN(x));
+export const getNumberEnumKeys = <T extends object>(numberEnum: T): Array<keyof T> => Object.values(numberEnum)
+    .filter((x) => isNaN(x));
 
 /**
  * Get the values from a Typescript number-Enum.
  * @param numberEnum - The enum to get the values from.
  */
-export const getNumberEnumValues = <T>(numberEnum: T): Array<T[keyof T]> => Object.values(numberEnum)
+export const getNumberEnumValues = <T extends object>(numberEnum: T): Array<T[keyof T]> => Object.values(numberEnum)
     .filter((x) => !isNaN(x)).map((x) => Number(x) as unknown as T[keyof T]);
